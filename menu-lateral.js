61b7d28787f451f77dc37308d74ce322ec8d9b8b
davirrocha <davirochamoraes7@gmail.com>
2026-05-27 23:30:29 -0300

    adicao-menu-lateral

---
+++ menu-lateral.js
@@ -0,0 +1,13 @@
+const mobileMenu = document.querySelector(".aside-categories")
+const btnMobileMenu = document.querySelector(".btn-menu")
+
+console.log(btnMobileMenu)
+
+btnMobileMenu.addEventListener("click", () => {
+    mobileMenu.classList.toggle("open")
+})
+
+
+
+
+
